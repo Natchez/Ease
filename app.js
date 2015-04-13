68ca9eec35be6e280d0d3daad04aa3cdcc2840f4
Aaron Fields-Cypress <natchez@stu.aii.edu>
2015-04-13 09:43:21 -0700

    WK1 Update
    Sign Up & Login logic with ionic completed

---
+++ app.js
@@ -0,0 +1,244 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var http = require('http');
+var path = require('path');
+var app = express();
+
+app.set('name', 'Ease');
+app.set('jwtTokenSecret', 'sj39f7dhkwl0d83hslsod827dhsids9');
+app.set('port', process.env.PORT || 3000);
+
+app.use('/', express.static(__dirname + '/www'));
+app.use(bodyParser.json());
+/////////////////////////////////////////// DATABASE CONNECTION POOL /////////////////////////////////////
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    multipleStatements: true
+});
+var connectionpool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '1991',
+    database: 'ease'
+});
+
+/////////////////////////////////////////// MIDDLEWARE//////////////////////////////////////////
+
+// middleware
+var jwtauth = function (req, res, next) {
+        var parsed_url = url.parse(req.url, true)
+        var token = (req.body && req.body.access_token) || parsed_url.query.access_token || req.headers["x-access-token"];
+        if (token) {
+            try {
+                var decoded = jwt.decode(token, app.get('jwtTokenSecret'))
+                if (decoded.exp <= Date.now()) {
+                    res.end('Access token has expired', 400)
+                }
+                // 'SELECT * FROM members WHERE idmembers = ' + decoded.iss
+                var query = connection.query('SELECT * from members WHERE idmembers = ??', [decoded.iss], function (err, user) {
+                    console.log(rows[0]);
+                    if (!err) {
+                        req.user = rows[0]
+                        return next()
+                    }
+                })
+            } catch (err) {
+                return next()
+            }
+        } else {
+            return next()
+        }
+    }
+    // It's one thing to authenticate, and it's another to require autentication
+var requireAuth = function (req, res, next) {
+    if (!req.user) {
+        res.json(401)
+    } else {
+        return next()
+    }
+}
+
+
+/////////////////////////////////////////// DISPLAY ALL MEMBERS IN DATEBASE /////////////////////////////////////////
+/*
+app.get('/members', function (req, res) {
+            var rows = '';
+            var query = connection.query('SELECT * FROM members', function(err, rows, result) {           
+                for(var i = 0; i < rows.length; i++)
+                    console.log('The solution is: ', rows[i].idmembers);
+                res.json(rows)
+});
+
+// show sql data grabbed from date base
+            console.log(query.sql);
+});
+app.get('/members/id', function (req, res) {
+     connectionpool.getConnection(function (err, connection) {
+        if (err) {
+            console.error('CONNECTION error: ', err);
+            res.statusCode = 503;
+              res.send({
+                result: 'error',
+                err:    err.code
+            });
+        } else {
+            var rows = "";
+            var query = connection.query('SELECT * FROM members', function(err, rows, result) {           
+                for(var i = 0; i < rows.length; i++)
+                    console.log('The solution is: ', rows[i].idmembers);
+                res.json(rows)
+});
+
+// show sql data grabbed from date base
+            console.log(query.sql);
+            connection.release();
+        }
+    });
+});
+
+/////////////////////////////////////////// DISPLAY ALL CLIENTS IN DATABASE /////////////////////////////////////////
+
+
+app.get('/clients', function (req, res) {
+     connectionpool.getConnection(function (err, connection) {
+        if (err) {
+            console.error('CONNECTION error: ', err);
+            res.statusCode = 503;
+              res.send({
+                result: 'error',
+                err:    err.code
+            });
+        } else {
+            var rows = '';
+            var query = connection.query('SELECT * FROM clients', function(err, rows, result) {
+               
+                for(var i = 0; i < rows.length; i++)
+                console.log('The solution is: ', rows[i].idclients);
+                res.json(rows)
+});
+
+// show sql data grabed from data base
+            console.log(query.sql);
+            connection.release();
+        }
+    });
+});
+
+/////////////////////////////////////////// DISPLAY ALL ARCHIVES IN DATABASE /////////////////////////////////////////
+
+
+app.get('/archive', function (req, res) {
+     connectionpool.getConnection(function (err, connection) {
+        if (err) {
+            console.error('CONNECTION error: ', err);
+            res.statusCode = 503;
+              res.send({
+                result: 'error',
+                err:    err.code
+            });
+        } else {
+            var rows = '';
+            var query = connection.query('SELECT * FROM archive', function(err, rows, result) {
+               
+                for(var i = 0; i < rows.length; i++)
+                console.log('The solution is: ', rows[i].idclients);
+                res.json(rows)
+});
+
+// show sql data grabed from data base
+            console.log(query.sql);
+            connection.release();
+        }
+    });
+});*/
+
+/////////////////////////////////////////// USER LOGIN ////////////////////////////////////
+
+app.post('/authenticate', function (req, res) {
+    connectionpool.getConnection(function (err, connection) {
+        if (err) {
+            console.error('CONNECTION error: ', err);
+            res.statusCode = 503;
+            res.send({
+                result: 'error',
+                err: err.code
+            });
+        } else {
+            connection.query('SELECT * from user WHERE username = ? AND password = ?', [(req.body.username), (req.body.password)], function (err, user) {
+                console.log(user)
+                if (typeof user == 'undefined' || user.length == 0) {
+                    res.status(401).json({
+                        message: 'Incorrect Username/Password'
+                    });
+                } else {
+                    res.status(200).json({
+                        message: 'Logged In',
+                        user: user
+                    });
+                }
+            });
+        }
+        console.log(req.body.username)
+        console.log(req.body.password)
+    });
+});
+
+
+
+
+/////////////////////////////////////////// CREATE USER STUFFS ////////////////////////////////////
+
+app.post('/addUser', function (req, res, next) {
+connectionpool.getConnection(function (err, connection) {
+    if (err) {
+        console.error('CONNECTION error: ', err);
+        res.statusCode = 503;
+        res.send({
+            result: 'error',
+            err: err.code
+        });
+    } else {
+        var member = req.body;
+        connection.query('INSERT INTO user SET ?', member, function (err, result) {
+            // error saving user in db
+            if (err)
+                res.status(401).json(err);
+            else
+                res.status(201).json({
+                    message: 'Account Created'
+                });
+        });
+    }
+});
+});
+
+/*
+{username:"asdaddsd",
+fname:"hjgh",
+street:"ghj",
+state:"GA",
+lname:"asd"
+,password:"dsa",
+city:"dsadsadsa",
+zip:12345,company:
+"dsadsadasddsa",phone:"
+5464"
+,email:"4561",
+lrate:123,
+mrate:456,
+hrate:789}'
+'*/
+
+
+
+
+/////////////////////////////////////////// CREATE SERVER STUFFS /////////////////////////////////////
+
+http
+    .createServer(app)
+    .listen(
+        app.get('port'),
+        "127.0.0.1"
+    );
+console.log("Ease Up on PORT 3000");
--- www/js/app.js
+++ www/js/app.js
@@ -6,66 +6,75 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
 
-  .state('app', {
-    url: "/app",
-    abstract: true,
-    templateUrl: "templates/menu.html",
-    controller: 'AppCtrl'
-  })
+        .state('app', {
+            url: "/app",
+            abstract: true,
+            templateUrl: "templates/menu.html"
+        })
+        .state('app.login', {
+            url: "/login",
+            views: {
+                'menuContent': {
+                    templateUrl: "templates/login.html",
+                    controller: "loginCtrl"
+                }
+            }
+        })
+        .state('app.signup', {
+            url: "/signup",
+            views: {
+                'menuContent': {
+                    templateUrl: "templates/signup.html",
+                    controller: "signupCtrl"
+                }
+            }
+        })
+        .state('app.profile', {
+            url: "/profile",
+            views: {
+                'menuContent': {
+                    templateUrl: "templates/profile.html",
+                    controller: "profileCtrl"
+                    
+                }
+            }
+        })
+        .state('app.clients', {
+            url: "/clients",
+            views: {
+                'menuContent': {
+                    templateUrl: "templates/clients.html",
+                    controller: "clientCtrl"
+                }
+            }
+        })
+        .state('app.archive', {
+            url: "/archive",
+            views: {
+                'menuContent': {
+                    templateUrl: "templates/archive.html",
+                    controller: "archiveCtrl"
+                }
+            }
+        });
 
-  .state('app.search', {
-    url: "/search",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/search.html"
-      }
-    }
-  })
-
-  .state('app.browse', {
-    url: "/browse",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/browse.html"
-      }
-    }
-  })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: "/playlists/:playlistId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/playlist.html",
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/login');
 });
